55f5842670d6c3224ca82d80a0c73083c267d1b4
Yeongmin Kim <oinochoe@naver.com>
2019-09-29 15:50:11 +0900

    좋다 기본 세이브시 포매터 작동

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          {/* 기본설정에서 format on save 옵션을 켰더니.. 자동으로 저장할 때마다 포맷터가 작동한다 */}
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
