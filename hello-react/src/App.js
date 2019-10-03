0608907959c2cff4c5b9e76e42cdc95493244aa2
Yeongmin Kim <oinochoe@naver.com>
2019-10-03 16:24:18 +0900

    기본 세팅 및 연습

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,27 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MyComponent from './MyComponent';
+import Counter from './Counter';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {/* 기본설정에서 format on save 옵션을 켰더니.. 자동으로 저장할 때마다 포맷터가 작동한다 */}
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <MyComponent name={'영민'} favoriteNumber={3}>
+        리액트
+      </MyComponent>
+      <Counter />
+    </>
   );
-}
+};
 
 export default App;
