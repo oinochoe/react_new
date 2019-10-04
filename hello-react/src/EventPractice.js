8bed48072aea1e53db3f81e4a366181c179c8402
Yeongmin Kim <oinochoe@naver.com>
2019-10-04 22:35:06 +0900

    e.target.name을 활용한 useState

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -1,14 +1,24 @@
 import React, { useState } from 'react';
 
 const EventPractice = () => {
-  const [username, setUsername] = useState('');
-  const [message, setMessage] = useState('');
-  const onChangeUsername = e => setUsername(e.target.value);
-  const onChangeMessage = e => setMessage(e.target.value);
+  const [form, setForm] = useState({
+    username: '',
+    message: ''
+  });
+  const { username, message } = form;
+  const onChnage = e => {
+    const nextForm = {
+      ...form, // 기존의 form 내용을 이 자리에 복사한 뒤
+      [e.target.name]: e.target.value // 원하는 값을 덮어 씌우기
+    };
+    setForm(nextForm);
+  };
   const onClick = () => {
     alert(username + ': ' + message);
-    setUsername('');
-    setMessage('');
+    setForm({
+      username: '',
+      message: ''
+    });
   };
   const onKeyPress = e => {
     if (e.key === 'Enter') {
@@ -23,14 +33,15 @@ const EventPractice = () => {
         name="username"
         placeholder="사용자명"
         value={username}
-        onChange={onChangeUsername}
+        onChange={onChnage}
+        onKeyPress={onKeyPress}
       />
       <input
         type="text"
         name="message"
         placeholder="아무거나 입력해 보세요"
         value={message}
-        onChange={onChangeMessage}
+        onChange={onChnage}
         onKeyPress={onKeyPress}
       />
       <button onCLick={onClick}>확인</button>
