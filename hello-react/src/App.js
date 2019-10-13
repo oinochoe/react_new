58d7dc9c0fbc8f368e80209f5b1c78129d0d7315
Yeongmin Kim <oinochoe@naver.com>
2019-10-13 16:10:52 +0900

    react ref 사용하기

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import MyComponent from './MyComponent';
 import Counter from './Counter';
 import Say from './Say';
@@ -6,19 +6,24 @@ import EventPractice from './EventPractice';
 import ValidationSample from './ValidationSample';
 import ScrollBox from './ScrollBox';
 
-const App = () => {
-  return (
-    <>
-      <MyComponent name={'영민'} favoriteNumber={3}>
-        리액트
-      </MyComponent>
-      <Counter />
-      <Say />
-      <EventPractice />
-      <ValidationSample />
-      <ScrollBox />
-    </>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <>
+        <MyComponent name={'영민'} favoriteNumber={3}>
+          리액트
+        </MyComponent>
+        <Counter />
+        <Say />
+        <EventPractice />
+        <ValidationSample />
+        <ScrollBox ref={ref => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 아래로
+        </button>
+      </>
+    );
+  }
+}
 
 export default App;
--- hello-react/src/ScrollBox.js
+++ hello-react/src/ScrollBox.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 
 class ScrollBox extends Component {
+  scrollToBottom = () => {
+    const { scrollHeight, clientHeight } = this.box;
+    /* 앞 코드에는 비구조화 할당 문법을 사용했습니다.
+     * 다음 코드와 같은 의미입니다.
+     * const scrollHeight = this.box.scrollHeight;
+     * const clientHeight = this.box.clientHeight;
+     */
+    this.box.scrollTop = scrollHeight - clientHeight;
+  };
+
   render() {
     const style = {
       border: '1px solid black',
