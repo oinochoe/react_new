38718c8a6d50f06da8b16043a35284d7bd451d64
Yeongmin Kim <oinochoe@naver.com>
2019-10-20 23:48:05 +0900

    life cycle method end

--- hello-react/src/LifeCycleSample.js
+++ hello-react/src/LifeCycleSample.js
@@ -13,14 +13,65 @@ class LifeCycleSample extends Component {
     console.log('constructor');
   }
 
-  static static getDerivedStateFromProps(nextProps, prevState) {
-    conosle.log('getDerivedStateFromProps');
-    if(nextProps.color !== prevState.color) {
+  static getDerivedStateFromProps(nextProps, prevState) {
+    console.log('getDerivedStateFromProps');
+    if (nextProps.color !== prevState.color) {
+      return { color: nextProps.color };
+    }
+    return null;
+  }
 
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('shouldComponentUpdate', nextProps, nextState);
+    // 숫자의 마지막 자리가 4면 렌더링하지 않습니다.
+    return nextState.number % 10 !== 4;
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+  }
+
+  handleClick = () => {
+    this.setState({
+      number: this.state.number + 1
+    });
+  };
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('getSnapshotBeforeUpdate');
+    if (prevProps.color !== this.props.color) {
+      return this.myRef.style.color;
+    }
+    return null;
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('componentDidUpdate', prevProps, prevState);
+    if (snapshot) {
+      console.log('업데이트가 되기 직전 색상', snapshot);
     }
   }
+
   render() {
-    return <div></div>;
+    console.log('render');
+
+    const style = {
+      color: this.props.color
+    };
+
+    return (
+      <div>
+        <h1 style={style} ref={ref => (this.myRef = ref)}>
+          {this.state.number}
+        </h1>
+        <p>color: {this.state.color}</p>
+        <button onClick={this.handleClick}>더하기</button>
+      </div>
+    );
   }
 }
 
