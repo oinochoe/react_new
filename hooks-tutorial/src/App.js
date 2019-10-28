e85049cfd6d10728e4ec5ca3e08185865fbfc325
Yeongmin Kim <oinochoe@naver.com>
2019-10-28 19:05:37 +0900

    리액트 hook인 useEffect 사용

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Counter from "./Counter";
+import Info from "./Info";
 
 function App() {
   return (
     <>
       <Counter />
+      <Info />
     </>
   );
 }
---
+++ hooks-tutorial/src/Info.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+
+const Info = () => {
+  const [name, setName] = useState("");
+  const [nickname, setNickname] = useState("");
+
+  useEffect(() => {
+    console.log("렌더링이 완료");
+    console.log({
+      name,
+      nickname
+    });
+  });
+
+  const onChangeName = e => {
+    setName(e.target.value);
+  };
+
+  const onChangeNickname = e => {
+    setNickname(e.target.value);
+  };
+
+  return (
+    <div>
+      <div>
+        <input vlaue={name} onChange={onChangeName} />
+        <input value={nickname} onChange={onChangeNickname} />
+      </div>
+      <div>
+        <div>
+          <b>이름 : {name}</b>
+        </div>
+        <div>
+          <b>닉네임 : </b> {nickname}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
