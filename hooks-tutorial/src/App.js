82ba6178d516fef2e69ce03c06611c9d56752eeb
Yeongmin Kim <oinochoe@naver.com>
2019-11-10 02:02:34 +0900

    useMemo hook 사용하여 평균값 계싼

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import Counter from "./Counter";
 import Info from "./Info";
+import Average from "./Average";
 
 const App = () => {
   const [visible, setVisible] = useState(false);
   return (
     <>
+      <Average />
       <button
         onClick={() => {
           setVisible(!visible);
