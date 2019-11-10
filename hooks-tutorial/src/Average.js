35fe0efb9ef3201a2e774ca2477f0eaaafae6000
Yeongmin Kim <oinochoe@naver.com>
2019-11-10 16:39:24 +0900

    필요할 때만 함수 생성하도록 useCallbkack 사용

--- hooks-tutorial/src/Average.js
+++ hooks-tutorial/src/Average.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useMemo, useCallback } from "react";
 
 const getAverage = numbers => {
   console.log("평균값 계산중..");
@@ -11,14 +11,18 @@ const Average = () => {
   const [list, setList] = useState([]);
   const [number, setNumber] = useState("");
 
-  const onChange = e => {
+  const onChange = useCallback(e => {
     setNumber(e.target.value);
-  };
-  const onInsert = e => {
-    const nextList = list.concat(parseInt(number));
-    setList(nextList);
-    setNumber("");
-  };
+  }, []); // 컴포넌트가 처음 렌더링 될 때만 함수 생성
+
+  const onInsert = useCallback(
+    e => {
+      const nextList = list.concat(parseInt(number));
+      setList(nextList);
+      setNumber("");
+    },
+    [number, list]
+  ); // number 혹은 list가 바뀌었을 때만 함수 생성
 
   const avg = useMemo(() => getAverage(list), [list]);
 
