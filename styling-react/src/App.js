6ee07a2cf82bfb86c61706cd10650c06d0473b04
Yeongmin Kim <oinochoe@naver.com>
2019-11-10 19:54:16 +0900

    sass 사용

---
+++ styling-react/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SassComponent from "./SassComponent";
+
+function App() {
+  return (
+    <>
+      <SassComponent />
+    </>
+  );
+}
+
+export default App;
---
+++ styling-react/src/SassComponent.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./SassComponent.scss";
+
+const SassComponent = () => {
+  return (
+    <div className="SassComponent">
+      <div className="box red" />
+      <div className="box orange" />
+      <div className="box yellow" />
+      <div className="box green" />
+      <div className="box blue" />
+    </div>
+  );
+};
+
+export default SassComponent;
