c80b921c36beebf6ed071cd1cab0b518bfcc39ea
Yeongmin Kim <oinochoe@naver.com>
2019-11-10 18:46:56 +0900

    useRef hook사용 및 custom hooks로 만들어 컴포넌트에서 사용

--- hooks-tutorial/src/Average.js
+++ hooks-tutorial/src/Average.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useCallback } from "react";
+import React, { useState, useMemo, useCallback, useRef } from "react";
 
 const getAverage = numbers => {
   console.log("평균값 계산중..");
@@ -10,6 +10,7 @@ const getAverage = numbers => {
 const Average = () => {
   const [list, setList] = useState([]);
   const [number, setNumber] = useState("");
+  const inputEl = useRef(null);
 
   const onChange = useCallback(e => {
     setNumber(e.target.value);
@@ -20,6 +21,7 @@ const Average = () => {
       const nextList = list.concat(parseInt(number));
       setList(nextList);
       setNumber("");
+      inputEl.current.focus();
     },
     [number, list]
   ); // number 혹은 list가 바뀌었을 때만 함수 생성
@@ -28,7 +30,7 @@ const Average = () => {
 
   return (
     <div>
-      <input value={number} onChange={onChange} />
+      <input value={number} onChange={onChange} ref={inputEl} />
       <button onClick={onInsert}>등록</button>
       <ul>
         {list.map((value, index) => (
--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -1,18 +1,13 @@
-import React, { useReducer } from "react";
-
-function reducer(state, action) {
-  return {
-    ...state,
-    [action.name]: action.value
-  };
-}
+import React from "react";
+import useInputs from "./useInputs";
 
 const Info = () => {
-  const [state, dispatch] = useReducer(reducer, { name: "", nickname: "" });
+  const [state, onChange] = useInputs({
+    name: "",
+    nickname: ""
+  });
+
   const { name, nickname } = state;
-  const onChange = e => {
-    dispatch(e.target);
-  };
 
   return (
     <div>
