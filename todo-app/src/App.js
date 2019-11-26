c0c811c42b19a8d63bdd255337d8bde34a4f6dc4
Yeongmin Kim <oinochoe@naver.com>
2019-11-27 02:24:13 +0900

    불러오는 컴포넌트 파일이 열려있찌 않아도 자동완성되는 jsconf.json 파일 작성

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import TodoTemplate from './components/TodoTemplate';
 
 const App = () => {
-  return <div>Todo App을 만들자</div>;
+  return <TodoTemplate>Todo App을 만들자</TodoTemplate>;
 };
 
 export default App;
