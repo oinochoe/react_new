bfb547144a93d7889f4410e793dd764173f0fc4e
Yeongmin Kim <oinochoe@naver.com>
2020-01-02 01:46:53 +0900

    불변성 유지하면서 immer 사용하지 않고 업데이트하기

--- immer-tutorial/src/App.js
+++ immer-tutorial/src/App.js
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useCallback, useState } from 'react';
 
-function App() {
+const App = () => {
+  const nextId = useRef(1);
+  const [form, setForm] = useState({ name: '', username: '' });
+  const [data, setData] = useState({
+    array: [],
+    uselessValue: null,
+  });
+
+  // input 수정을 위한 함수
+  const onChange = useCallback(
+    e => {
+      const { name, value } = e.target;
+      setForm({
+        ...form,
+        [name]: [value],
+      });
+    },
+    [form],
+  );
+
+  // form 등록을 위한 함수
+  const onSubmit = useCallback(
+    e => {
+      e.preventDefault();
+      const info = {
+        id: nextId.current,
+        name: form.name,
+        username: form.username,
+      };
+
+      // array에 새 항목 등록
+      setData({
+        ...data,
+        array: data.array.concat(info),
+      });
+
+      // form 초기화
+      setForm({
+        name: '',
+        username: '',
+      });
+      nextId.current += 1;
+    },
+    [data, form.name, form.username],
+  );
+
+  // 항목을 삭제하는 함수
+  const onRemove = useCallback(
+    id => {
+      setData({
+        ...data,
+        array: data.array.filter(info => info.id !== id),
+      });
+    },
+    [data],
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="username"
+          placeholder="아이디"
+          value={form.username}
+          onChange={onChange}
+        />
+        <input
+          name="name"
+          placeholder="이름"
+          value={form.name}
+          onChange={onChange}
+        />
+        <button type="submit">등록</button>
+      </form>
+      <div>
+        <ul>
+          {data.array.map(info => (
+            <li key={info.id} onClick={() => onRemove(info.id)}>
+              {info.username} ({info.name})
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
