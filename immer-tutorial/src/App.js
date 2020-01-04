8e23e7f49d393503ee53a3d3f246b7ec2a40cb75
Yeongmin Kim <oinochoe@naver.com>
2020-01-04 21:49:46 +0900

    useState의 함수형 업데이트를 사용하여 immer를 더욱 깔끔하게

--- immer-tutorial/src/App.js
+++ immer-tutorial/src/App.js
@@ -10,17 +10,14 @@ const App = () => {
   });
 
   // input 수정을 위한 함수
-  const onChange = useCallback(
-    e => {
-      const { name, value } = e.target;
-      setForm(
-        produce(form, draft => {
-          draft[name] = value;
-        }),
-      );
-    },
-    [form],
-  );
+  const onChange = useCallback(e => {
+    const { name, value } = e.target;
+    setForm(
+      produce(draft => {
+        draft[name] = value;
+      }),
+    );
+  }, []);
 
   // form 등록을 위한 함수
   const onSubmit = useCallback(
@@ -34,7 +31,7 @@ const App = () => {
 
       // array에 새 항목 등록
       setData(
-        produce(data, draft => {
+        produce(draft => {
           draft.array.push(info);
         }),
       );
@@ -46,23 +43,20 @@ const App = () => {
       });
       nextId.current += 1;
     },
-    [data, form.name, form.username],
+    [form.name, form.username],
   );
 
   // 항목을 삭제하는 함수
-  const onRemove = useCallback(
-    id => {
-      setData(
-        produce(data, draft => {
-          draft.array.splice(
-            draft.array.findIndex(info => info.id === id),
-            1,
-          );
-        }),
-      );
-    },
-    [data],
-  );
+  const onRemove = useCallback(id => {
+    setData(
+      produce(draft => {
+        draft.array.splice(
+          draft.array.findIndex(info => info.id === id),
+          1,
+        );
+      }),
+    );
+  }, []);
 
   return (
     <div>
