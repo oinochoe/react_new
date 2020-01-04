3f16b692742ce9ecc773e24a3ec80a394d2ef401
Yeongmin Kim <oinochoe@naver.com>
2020-01-04 21:24:09 +0900

    immer produce 함수 적용

--- immer-tutorial/src/App.js
+++ immer-tutorial/src/App.js
@@ -1,4 +1,5 @@
 import React, { useRef, useCallback, useState } from 'react';
+import produce from 'immer';
 
 const App = () => {
   const nextId = useRef(1);
@@ -12,10 +13,11 @@ const App = () => {
   const onChange = useCallback(
     e => {
       const { name, value } = e.target;
-      setForm({
-        ...form,
-        [name]: [value],
-      });
+      setForm(
+        produce(form, draft => {
+          draft[name] = value;
+        }),
+      );
     },
     [form],
   );
@@ -31,10 +33,11 @@ const App = () => {
       };
 
       // array에 새 항목 등록
-      setData({
-        ...data,
-        array: data.array.concat(info),
-      });
+      setData(
+        produce(data, draft => {
+          draft.array.push(info);
+        }),
+      );
 
       // form 초기화
       setForm({
@@ -49,10 +52,14 @@ const App = () => {
   // 항목을 삭제하는 함수
   const onRemove = useCallback(
     id => {
-      setData({
-        ...data,
-        array: data.array.filter(info => info.id !== id),
-      });
+      setData(
+        produce(data, draft => {
+          draft.array.splice(
+            draft.array.findIndex(info => info.id === id),
+            1,
+          );
+        }),
+      );
     },
     [data],
   );
