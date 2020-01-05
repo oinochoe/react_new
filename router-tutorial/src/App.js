bb0e8d2b67b169846efac96baad3006b76d3e179
Yeongmin Kim <oinochoe@naver.com>
2020-01-05 21:44:48 +0900

    url 파라미터 사용하여 값 넘기기

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,7 +1,8 @@
-import React from "react";
-import { Route, Link } from "react-router-dom";
-import About from "./About";
-import Home from "./Home";
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
+import About from './About';
+import Home from './Home';
+import Profile from './Profile';
 
 function App() {
   return (
@@ -13,9 +14,16 @@ function App() {
         <li>
           <Link to="/about">소개페이지</Link>
         </li>
+        <li>
+          <Link to="/profile/velopert">velopert 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profile/gildong">길동 프로필</Link>
+        </li>
       </ul>
       <Route path="/" component={Home} exact={true} />
-      <Route path={["/about", "/info"]} component={About} />
+      <Route path={['/about', '/info']} component={About} />
+      <Route path="/profile/:username" component={Profile} />
     </>
   );
 }
