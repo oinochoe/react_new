d5afc95e824e62d612d36d769582486a1d880282
Yeongmin <copstyle86@gmail.com>
2020-01-07 10:00:02 +0900

    서브라우트 연결 완료

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
-import Profile from './Profile';
+import Profiles from './Profiles';
 
 function App() {
   return (
@@ -15,15 +15,12 @@ function App() {
           <Link to="/about">소개페이지</Link>
         </li>
         <li>
-          <Link to="/profile/velopert">velopert 프로필</Link>
-        </li>
-        <li>
-          <Link to="/profile/gildong">길동 프로필</Link>
+          <Link to="/profiles">프로필</Link>
         </li>
       </ul>
       <Route path="/" component={Home} exact={true} />
       <Route path={['/about', '/info']} component={About} />
-      <Route path="/profile/:username" component={Profile} />
+      <Route path="/profiles" component={Profiles} />
     </>
   );
 }
