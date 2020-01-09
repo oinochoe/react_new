0345a80b3941c730ae8e1f766c6d8cf80e1a58fb
oinochoe <oinochoe@naver.com>
2020-01-09 09:12:04 +0900

    history api 사용

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -3,6 +3,7 @@ import { Route, Link } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
 import Profiles from './Profiles';
+import HistorySample from './HistorySample';
 
 function App() {
   return (
@@ -17,10 +18,14 @@ function App() {
         <li>
           <Link to="/profiles">프로필</Link>
         </li>
+        <li>
+          <Link to="/history">History 예제</Link>
+        </li>
       </ul>
       <Route path="/" component={Home} exact={true} />
       <Route path={['/about', '/info']} component={About} />
       <Route path="/profiles" component={Profiles} />
+      <Route path="/history" component={HistorySample} />
     </>
   );
 }
