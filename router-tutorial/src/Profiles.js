19dbff156328a145578f6e5b25c4fc471898cc63
Yeongmin Kim <oinochoe@naver.com>
2020-01-11 15:12:25 +0900

    history, match, location 접근 가능한 withRouter함수 만들기

--- router-tutorial/src/Profiles.js
+++ router-tutorial/src/Profiles.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, Route } from 'react-router-dom';
 import Profile from './Profile';
+import WithRouterSample from './WithRouterSample';
 
 const Profiles = () => {
   return (
@@ -21,6 +22,7 @@ const Profiles = () => {
         render={() => <div>사용자를 선택해주세요.</div>}
       />
       <Route path="/profiles/:username" component={Profile} />
+      <WithRouterSample />
     </div>
   );
 };
