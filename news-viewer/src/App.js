3f93053860715f9dbee2563f15e5acf95cfc0e53
Yeongmin Kim <oinochoe@naver.com>
2020-01-11 20:17:22 +0900

    async 추가

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -3,10 +3,15 @@ import axios from 'axios';
 
 const App = () => {
   const [data, setData] = useState(null);
-  const onClick = () => {
-    axios.get('https://jsonplaceholder.typicode.com/todos/1').then(response => {
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/todos/1',
+      );
       setData(response.data);
-    });
+    } catch (e) {
+      console.log(e);
+    }
   };
   return (
     <div>
