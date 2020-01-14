6a5b4ef82f7ae7ea7aaa487d32c8112826712d4c
Yeongmin Kim <oinochoe@naver.com>
2020-01-15 02:04:32 +0900

    상태관리할 수 있도록 category hook 사용

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import NewsList from './components/NewsList';
 import Categories from './components/Categories';
 
 const App = () => {
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback(category => setCategory(category), []);
+
   return (
     <>
-      <Categories />
-      <NewsList />
+      <Categories category={category} onSelect={onSelect} />
+      <NewsList category={category} />
     </>
   );
 };
