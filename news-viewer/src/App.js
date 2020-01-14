4d8083a1372cb94e070285bc79b91da10f737124
Yeongmin Kim <oinochoe@naver.com>
2020-01-15 01:55:23 +0900

    카테고리기능추가

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import NewsList from './components/NewsList';
+import Categories from './components/Categories';
 
 const App = () => {
-  return <NewsList />;
+  return (
+    <>
+      <Categories />
+      <NewsList />
+    </>
+  );
 };
 
 export default App;
