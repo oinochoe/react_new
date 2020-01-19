5d5e6f47c0ad23a6e1e71f1a23bdf016cf2abde4
Yeongmin Kim <oinochoe@naver.com>
2020-01-20 00:43:50 +0900

    초기화 및 ContextAPI 기초 사용

---
+++ context-tutorial/src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App() {
+  return <>뭐라고</>;
+}
+
+export default App;
---
+++ context-tutorial/src/components/ColorBox.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ColorContext from '../contexts/color';
+
+const ColorBox = () => {
+  return (
+    <ColorContext.Consumer>
+      {value => (
+        <div
+          style={{
+            width: '64px',
+            height: '64px',
+            background: value.color,
+          }}
+        />
+      )}
+    </ColorContext.Consumer>
+  );
+};
+
+export default ColorBox;
