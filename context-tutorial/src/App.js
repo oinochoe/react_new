77a6c055d183294d96f46250df62ebf9edb6c84f
oinochoe <oinochoe@naver.com>
2020-01-20 09:59:42 +0900

    Provider 제공

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ColorBox from './components/ColorBox';
+import ColorContext from './contexts/color';
 
-function App() {
+const App = () => {
   return (
-    <>
-      <ColorBox />
-    </>
+    <ColorContext.Provider value={{ color: 'red' }}>
+      <>
+        <ColorBox />
+      </>
+    </ColorContext.Provider>
   );
-}
+};
 
 export default App;
