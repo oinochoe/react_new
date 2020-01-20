a71dc61f63601fe487c511ccad88971192293417
Yeongmin Kim <oinochoe@naver.com>
2020-01-21 00:50:50 +0900

    색상 팔레트 추가

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ColorBox from './components/ColorBox';
 import { ColorProvider } from './contexts/color';
+import SelectColors from './components/SelectColors';
 
 const App = () => {
   return (
     <ColorProvider>
       <>
+        <SelectColors />
         <ColorBox />
       </>
     </ColorProvider>
