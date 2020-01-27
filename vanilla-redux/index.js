67dd727e2649339c54e46236c5ca676b54519260
Yeongmin Kim <oinochoe@naver.com>
2020-01-27 23:08:04 +0900

    액션 생성 함수 및 액션 설정

--- vanilla-redux/index.js
+++ vanilla-redux/index.js
@@ -1,1 +1,12 @@
-console.log('hello redux');
+const divToggle = document.querySelector('.toggle');
+const counter = document.querySelector('h1');
+const btnIncrease = document.querySelector('#increase');
+const btnDecrease = document.querySelector('#decrease');
+
+const TOGGLE_SWITCH = 'TOGGLE_SWITCH';
+const INCREASE = 'INCREASE';
+const DECREASE = 'DECREASE';
+
+const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
+const increase = difference => ({ type: INCREASE, difference });
+const decrease = () => ({ type: DECREASE });
