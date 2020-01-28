6d39455cb9893100fdd00f0a9f92c3eb7c94e230
Yeongmin Kim <oinochoe@naver.com>
2020-01-29 00:48:31 +0900

    state에 따른 reducer 함수와 store 만들기

--- vanilla-redux/index.js
+++ vanilla-redux/index.js
@@ -1,3 +1,5 @@
+import { createStore } from 'redux';
+
 const divToggle = document.querySelector('.toggle');
 const counter = document.querySelector('h1');
 const btnIncrease = document.querySelector('#increase');
@@ -11,8 +13,35 @@ const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
 const increase = difference => ({ type: INCREASE, difference });
 const decrease = () => ({ type: DECREASE });
 
+// 스토어 만들기
+const store = createStore(reducer);
+
 // 초깃값 설정
 const initialState = {
   toggle: false,
   counter: 0,
 };
+
+// state가 undefined일 때는 initialState를 기본값으로 사용
+function reducer(state = initialState, action) {
+  // action.type에 따라 다른 작업을 처리함.
+  switch (action.type) {
+    case TOGGLE_SWITCH:
+      return {
+        ...state, // 불변성 유지를 해 주어야 합니다.
+        toggle: !state.toggle,
+      };
+    case INCREASE:
+      return {
+        ...state,
+        counter: state.counter + action.difference,
+      };
+    case DECREASE:
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+    default:
+      return state;
+  }
+}
