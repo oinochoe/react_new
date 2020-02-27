afb90af8758d4a04d41a5f2a21ba46fd9ea5924a
Yeongmin Kim <oinochoe@naver.com>
2020-02-27 19:45:01 +0900

    공백 들여쓰기 변경

--- react-redux-tutorial_new/src/containers/TodosContainer.js
+++ react-redux-tutorial_new/src/containers/TodosContainer.js
@@ -3,39 +3,37 @@ import { connect } from 'react-redux';
 import { changeInput, insert, toggle, remove } from '../modules/todos';
 import Todos from '../components/Todos';
 
-
 const TodosContainer = ({
-  input,
-  todos,
-  changeInput,
-  insert,
-  toggle,
-  remove,
-}) => {
-  return (
-    <Todos
-      input={input}
-      todos={todos}
-      onChangeInput={changeInput}
-      onInsert={insert}
-      onToggle={toggle}
-      onRemove={remove}
-    />
-  );
-}
-
-
-export default connect(
-  // 비구조화 할당을 통해 toods를 분리하여
-  // state.todos.input 대신 todos.input을 사용
-  ({ todos }) => ({
-    input: todos.input,
-    todos: todos.todos
-  }),
-  {
+    input,
+    todos,
     changeInput,
     insert,
     toggle,
-    remove
-  }
+    remove,
+}) => {
+    return (
+        <Todos
+            input={input}
+            todos={todos}
+            onChangeInput={changeInput}
+            onInsert={insert}
+            onToggle={toggle}
+            onRemove={remove}
+        />
+    );
+};
+
+export default connect(
+    // 비구조화 할당을 통해 toods를 분리하여
+    // state.todos.input 대신 todos.input을 사용
+    ({ todos }) => ({
+        input: todos.input,
+        todos: todos.todos,
+    }),
+    {
+        changeInput,
+        insert,
+        toggle,
+        remove,
+    },
 )(TodosContainer);
