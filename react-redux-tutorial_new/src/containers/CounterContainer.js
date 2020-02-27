3d7817b5ec95233bf08a683ea9225e5a17cfa40f
Yeongmin Kim <oinochoe@naver.com>
2020-02-27 19:11:39 +0900

    증가 감소 useCallback hook으로 사용

--- react-redux-tutorial_new/src/containers/CounterContainer.js
+++ react-redux-tutorial_new/src/containers/CounterContainer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
@@ -6,11 +6,13 @@ import { increase, decrease } from '../modules/counter';
 const CounterContainer = () => {
     const number = useSelector(state => state.counter.number);
     const dispatch = useDispatch();
+    const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+    const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
     return (
         <Counter
             number={number}
-            onIncrease={() => dispatch(increase())}
-            onDecrease={() => dispatch(decrease())}
+            onIncrease={onIncrease}
+            onDecrease={onDecrease}
         />
     );
 };
