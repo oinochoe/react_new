b42630d7c2512fc9020474d66c49f4a34a9ae83e
oinochoe <oinochoe@naver.com>
2020-03-02 09:58:46 +0900

    리듀서 작성중

---
+++ learn-redux-middleware/src/modules/sample.js
@@ -0,0 +1,63 @@
+import { handleActions } from 'redux-actions';
+import * as api from '../lib/api';
+
+// 액션 타입을 선언합니다.
+// 한 요청당 세 개를 만들어야 합니다.
+
+const GET_POST = 'sample/GET_POST';
+const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS';
+const GET_POST_FAILURE = 'sample/GET_POST_FAILURE';
+
+const GET_USERS = 'sample/GET_USERS';
+const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCESS';
+const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
+
+// thunk 함수 생성
+// thunk 함수 내부에는 시작할 때, 성공했을 때, 실패했을 때 다른 액션을 디스패치 합니다.
+
+export const getPost = id => async dispatch => {
+    dispatch({ type: GET_POST }); // 요청을 시작한 것을 알림
+    try {
+        const response = await api.getPost(id);
+        dispatch({
+            type: GET_POST_SUCCESS,
+            payload: response.data,
+        }); // 요청 성공
+    } catch (e) {
+        dispatch({
+            type: GET_POST_FAILURE,
+            payload: e,
+            error: true,
+        }); // 에러 발생
+        throw e; // 나중에 컴포넌트단에서 에러를 조회할 수 있게 해줌
+    }
+};
+
+// 초기상태 선언
+// 요청의 로딩 중 상태는 Loading이라는 객체에서 관리합니다.
+
+const initialState = {
+    loading: {
+        GET_POST: false,
+        GET_USERS: false,
+    },
+    post: null,
+    users: null,
+};
+
+const sample = handleActions({
+    [GET_POST]: state => ({
+        ...state,
+        loading: {
+            ...state.loading,
+            GET_POST: true, // 요청 시작
+        },
+    }),
+    [GET_POST_SUCCESS]: (state, action) => ({
+        ...state,
+        loading: {
+            ...state.loading,
+            GET_POST: false, //요청 완료
+        },
+    }),
+});
