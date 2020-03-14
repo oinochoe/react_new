0f0e3417046fb969138b2e6298eeae2a51350abc
Yeongmin Kim <oinochoe@naver.com>
2020-03-14 16:23:16 +0900

    모듈로 인해 불필요 소스 제거

--- learn-redux-middleware/src/modules/sample.js
+++ learn-redux-middleware/src/modules/sample.js
@@ -7,11 +7,9 @@ import createRequestThunk from '../lib/createRequestThunk';
 
 const GET_POST = 'sample/GET_POST';
 const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS';
-const GET_POST_FAILURE = 'sample/GET_POST_FAILURE';
 
 const GET_USERS = 'sample/GET_USERS';
 const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCESS';
-const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 
 // thunk 함수 생성
 // thunk 함수 내부에는 시작할 때, 성공했을 때, 실패했을 때 다른 액션을 디스패치 합니다.
@@ -23,59 +21,20 @@ export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
 // 요청의 로딩 중 상태는 Loading이라는 객체에서 관리합니다.
 
 const initialState = {
-    loading: {
-        GET_POST: false,
-        GET_USERS: false,
-    },
     post: null,
     users: null,
 };
 
 const sample = handleActions(
     {
-        [GET_POST]: state => ({
-            ...state,
-            loading: {
-                ...state.loading,
-                GET_POST: true, // 요청 시작
-            },
-        }),
         [GET_POST_SUCCESS]: (state, action) => ({
             ...state,
-            loading: {
-                ...state.loading,
-                GET_POST: false, //요청 완료
-            },
             post: action.payload,
         }),
-        [GET_POST_FAILURE]: (state, action) => ({
-            ...state,
-            loading: {
-                ...state.loading,
-                GET_POST: false, // 요청 완료
-            },
-        }),
-        [GET_USERS]: state => ({
-            ...state,
-            loading: {
-                ...state.loading,
-                GET_USERS: true, // 요청 시작
-            },
-        }),
+
         [GET_USERS_SUCCESS]: (state, action) => ({
             ...state,
-            loading: {
-                ...state.loading,
-                GET_USERS: false, // 요청 완료
-            },
             users: action.payload,
-        }),
-        [GET_USERS_FAILURE]: (state, action) => ({
-            ...state,
-            loading: {
-                ...state.loading,
-                GET_USERS: false,
-            },
         }),
     },
     initialState,
