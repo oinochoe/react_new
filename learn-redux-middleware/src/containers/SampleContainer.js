e32e201e985b675a539dabe57c29e1877bcb6859
Yeongmin Kim <oinochoe@naver.com>
2020-03-14 16:30:19 +0900

    에러처리를 위함(훅 내에서는 async 파라미터로 받은 거 사용 못해서 따로 만듬)

--- learn-redux-middleware/src/containers/SampleContainer.js
+++ learn-redux-middleware/src/containers/SampleContainer.js
@@ -14,8 +14,17 @@ const SampleContainer = ({
 }) => {
     // 클래스 형태컴포넌트였다면 componentDidMount
     useEffect(() => {
-        getPost(1);
-        getUsers(1);
+        // useEffet에 파라미터로 넣는 함수는 async로 할 수 없기 때문에
+        // 그 내부에서 async 함수를 선언하고 호출해줍니다.
+        const fn = async () => {
+            try {
+                await getPost(1);
+                await getUsers(1);
+            } catch (e) {
+                console.log(e); // 에러 조회
+            }
+        };
+        fn();
     }, [getPost, getUsers]);
     return (
         <Sample
