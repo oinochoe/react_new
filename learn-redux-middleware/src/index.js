a5ca90bd5b33092d7137318c0359e6dd65c5f4da
Yeongmin Kim <oinochoe@naver.com>
2020-03-22 21:05:56 +0900

    redux saga 적용 및 devtools

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -4,12 +4,20 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import rootReducer from './modules';
+import rootReducer, { rootSaga } from './modules';
 import { createLogger } from 'redux-logger';
 import ReduxThunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const logger = createLogger();
-const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(logger, ReduxThunk, sagaMiddleware)),
+);
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
     <Provider store={store}>
