0d2e981216bc78559ab730b1d4e032a3d157308b
Yeongmin Kim <oinochoe@naver.com>
2020-03-25 00:47:00 +0900

    스토어 내부에서 상태 조회

--- learn-redux-middleware/src/modules/counter.js
+++ learn-redux-middleware/src/modules/counter.js
@@ -1,5 +1,5 @@
 import { createAction, handleActions } from 'redux-actions';
-import { delay, put, takeEvery, takeLatest } from 'redux-saga/effects';
+import { delay, put, takeEvery, takeLatest, select } from 'redux-saga/effects';
 
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
@@ -18,6 +18,8 @@ export const decreaseAsync = createAction(DECREASE_ASYNC, () => undefined);
 function* increaseSaga() {
     yield delay(1000); // 1초 기다림
     yield put(increase()); // 특정 액션을 디스패치
+    const number = yield select(state => state.counter); // state는 스토어 상태를 의미함
+    console.log(`현재 값은 ${number} 입니다.`);
 }
 
 function* decreaseSaga() {
