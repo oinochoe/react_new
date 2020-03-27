6293e252a1acb2874f33f079cf81b502a901d85b
yeongminkim <oinochoe@naver.com>
2020-03-28 00:19:54 +0900

    notify js

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import notify from "./notify";
 
 function App() {
+  const onClick = () => {
+    notify();
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello React</p>
+        <p onClick={onClick}>Hello React</p>
       </header>
     </div>
   );
