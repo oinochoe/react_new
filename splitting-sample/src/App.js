842f396ee5cf57b0fb3f31a89cfb04ff6f0c0ce5
yeongminkim <oinochoe@naver.com>
2020-03-28 01:29:24 +0900

    소스 정리

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,19 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import notify from "./notify";
-
 function App() {
   const onClick = () => {
-    notify();
+    import("./notify").then(result => result.default());
   };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={onClick}>Hello React</p>
-      </header>
-    </div>
+    <>
+      <p onClick={onClick}>Hello React</p>
+    </>
   );
 }
 
