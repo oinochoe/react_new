a9ef5d1d9527232137dcfabbe884efe8b28342a9
yeongminkim <oinochoe@naver.com>
2020-04-01 22:23:02 +0900

    manifest json이용하여 파일 뿌리기

--- ssr-recipe/src/index.server.js
+++ ssr-recipe/src/index.server.js
@@ -4,6 +4,39 @@ import express from 'express';
 import { StaticRouter } from 'react-router-dom';
 import App from './App';
 import path from 'path';
+import fs from 'fs';
+
+// asset-manifest.json에서 파일 경로들을 조회합니다.
+const manifest = JSON.parse(fs.readFileSync(path.resolve('./build/asset-manifest.json', 'utf8')));
+
+const chunks = Object.keys(manifest.files)
+    .filter(key => /chunk\.js$/.exec(key)) // chunk.js로 끝나는 키를 찾아서
+    .map(key => `<script src="${manifest[key]}></script>`) // script 태그로 변환하고
+    .join(''); // 합침
+
+function createPage(root) {
+    return `<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="utf-8" />
+            <link rel="shortcut icon" href="/favicon.ico" />
+            <meta name="viewport" content"width=device-width, initial-scale=1, shrink-to-fit=no" />
+            <meta name="theme-color" content="#000000">
+            <title>React App</title>
+            <link href="${manifest['main.css']}" rel="stylesheet" />
+        </head>
+        <body>
+            <noscript>You need to enable Javascript to run this app.</noscript>
+            <div id="root">
+                ${root}
+            </div>
+            <script src="${manifest['runtime-main.js']}></script>
+            ${chunks}
+            <script src="${manifest['main.js']}></script>
+        </body>
+    </html>
+    `;
+}
 
 const app = express();
 
@@ -18,7 +51,7 @@ const serverRender = (req, res, next) => {
         </StaticRouter>
     );
     const root = ReactDOMServer.renderToString(jsx); // 렌더링을 하고
-    res.send(root); // 클라이언트에 결과물을 응답합니다.
+    res.send(createPage(root)); // 클라이언트에 결과물을 응답합니다.
 };
 
 const serve = express.static(path.resolve('./build'), {
