ae4a11622689747e3fbf772b7ee1f87701a2b447
yeongminkim <oinochoe@naver.com>
2020-04-03 01:25:10 +0900

    초깃값 지정

--- ssr-recipe/src/index.js
+++ ssr-recipe/src/index.js
@@ -7,7 +7,11 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import rootReducer from './modules';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+    rootReducer,
+    window.__PRELOADED_STATE_, // 이 값을 초기 상태로 사용함...
+    applyMiddleware(thunk),
+);
 
 ReactDOM.render(
     <Provider store={store}>
