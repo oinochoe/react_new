3122394ee5b067b5be3f83ecc22be68aa9c566b6
yeongminkim <oinochoe@naver.com>
2020-04-04 00:31:25 +0900

    sagaMiddleware

--- ssr-recipe/src/index.js
+++ ssr-recipe/src/index.js
@@ -5,13 +5,18 @@ import { BrowserRouter } from 'react-router-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import rootReducer from './modules';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer, { rootSaga } from './modules';
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
     rootReducer,
     window.__PRELOADED_STATE_, // 이 값을 초기 상태로 사용함...
-    applyMiddleware(thunk),
+    applyMiddleware(thunk, sagaMiddleware),
 );
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
     <Provider store={store}>
