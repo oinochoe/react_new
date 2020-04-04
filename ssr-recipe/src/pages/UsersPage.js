a35a5bf0c8f836708ebeef41e00d29c0cac6670a
yeongminkim <oinochoe@naver.com>
2020-04-04 14:48:33 +0900

    user 컴포넌트 렌더링

--- ssr-recipe/src/pages/UsersPage.js
+++ ssr-recipe/src/pages/UsersPage.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import UsersContainer from '../containers/UsersContainer';
+import UserContainer from '../containers/UserContainer';
+import { Route } from 'react-router-dom';
 
 const UsersPage = () => {
-    return <UsersContainer />;
+    return (
+        <>
+            <UsersContainer />
+            <Route path='/users/:id' render={({ match }) => <UserContainer id={match.params.id} />} />
+        </>
+    );
 };
 
 export default UsersPage;
