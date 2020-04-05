a79f37f5b89b26deb7546e3cc9f114868e84c701
yeongminkim <oinochoe@naver.com>
2020-04-06 00:32:22 +0900

    load splitting

--- ssr-recipe/src/App.js
+++ ssr-recipe/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Menu from './components/Menu';
-import RedPage from './pages/RedPage';
-import BluePage from './pages/BluePage';
-import UsersPage from './pages/UsersPage';
+import loadable from '@loadable/component';
+const RedPage = loadable(() => import('./pages/RedPage'));
+const BluePage = loadable(() => import('./pages/BluePage'));
+const UsersPage = loadable(() => import('./pages/UsersPage'));
 
 const App = () => {
     return (
