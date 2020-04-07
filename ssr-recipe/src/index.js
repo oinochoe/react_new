14a0fbf89edc09b781595c934ca147f531da7cac
yeongminkim <oinochoe@naver.com>
2020-04-08 00:25:12 +0900

    loadable 완료

--- ssr-recipe/src/index.js
+++ ssr-recipe/src/index.js
@@ -7,6 +7,7 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer, { rootSaga } from './modules';
+import { loadableReady } from '@loadable/component';
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -18,11 +19,25 @@ const store = createStore(
 
 sagaMiddleware.run(rootSaga);
 
-ReactDOM.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Provider>,
-    document.getElementById('root'),
-);
+// 같은 내용을 쉽게 재사용할 수 있도록 렌더링할 내용을 하나의 컴포넌트로 묶음
+const Root = () => {
+    return (
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
+    );
+};
+
+const root = document.getElementById('root');
+
+// 프로덕션 환경에서는 loadableReady와 hydrate를 사용하고
+// 개발 환경에서는 기존 방식으로 처리
+if (process.env.NODE_ENV === 'production') {
+    loadableReady(() => {
+        ReactDOM.hydrate(<Root />, root);
+    });
+} else {
+    ReactDOM.render(<Root />, root);
+}
